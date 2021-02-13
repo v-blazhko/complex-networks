a2dfcdcbb25fdb1e27660dd5819f97670a9b0ca8
Veronika Blazhko <veronika.blazko@tuni.fi>
2021-02-13 20:28:33 +0200

    Geo API, many success

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ api_key = u'b766ae533b2843f748b174f53885eee4'
 api_secret = u'ac3884caa9690723'
 
 flickr = flickrapi.FlickrAPI(api_key, api_secret, format='json')
-photos = flickr.photos.search(tags='Tampere', per_page='10')
-
+photos = flickr.photos.search(per_page='100', tags='Finland,Suomi', has_geo=1)
 parsed = json.loads(photos.decode('utf-8'))
-print(parsed)
+
+## 6242 pages, 10 photos per page
+## 50932, 10p/p
+
+for item in parsed['photos']['photo']:
+    print(item['title'])
+id = parsed['photos']['photo'][0]['id']
+geo = flickr.do_flickr_call('flickr.photos.geo.getLocation', photo_id=id)
+print(json.loads(geo.decode('utf-8')))
