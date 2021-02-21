47e42f594f71f338329b48b1590987ca929a85c1
Veronika Blazhko <veronika.blazko@tuni.fi>
2021-02-21 19:33:23 +0200

    Geo API, many success

--- main.py
+++ main.py
@@ -5,30 +5,30 @@ api_key = u'b766ae533b2843f748b174f53885eee4'
 api_secret = u'ac3884caa9690723'
 
 flickr = flickrapi.FlickrAPI(api_key, api_secret, format='json')
-# Get all pictures from Finland
-photos = flickr.photos.search(per_page='100', tags='Finland,Suomi', has_geo=1)
+photos = flickr.photos.search(per_page='250', bbox='60.562697, 21.402542, 69.766582, 28.664383', has_geo=1)
 parsed = json.loads(photos.decode('utf-8'))
+
 
 ## 6242 pages, 10 photos per page
 ## 50932, 10p/p
 ## id | location | name | tags
 
-for item in parsed['photos']['photo']:
-    print(item['title'])
-id = parsed['photos']['photo'][0]['id']
-geo = flickr.do_flickr_call('flickr.photos.geo.getLocation', photo_id=id)
-print(json.loads(geo.decode('utf-8')))
-
+photos_set = []
+for i in range(1,40):
+    res = flickr.photos.search(per_page='250', bbox='60.562697, 21.402542, 69.766582, 28.664383', has_geo=1)
+    parsed = json.loads(photos.decode('utf-8'))
+    for p in parsed['photos']['photo']:
+        photos_set.append(photos)
 
 # 50 lat x 50 lon -> at least 500 squares
 
-for lat in range(latidudes):
-    for lon in range(longitudes):
-        photos = flickr.photos.search(per_page='100', lat=lat, lon=lon, accuracy=10, has_geo=1)
-        users_visited = []
-        parsed = json.loads(photos.decode('utf-8'))
-        id = parsed['photos']['photo'][0]['id']
-        geo = flickr.do_flickr_call('flickr.photos.geo.getLocation', photo_id=id)
+# for lat in range(latidudes):
+#     for lon in range(longitudes):
+#         photos = flickr.photos.search(per_page='100', lat=lat, lon=lon, accuracy=10, has_geo=1)
+#         users_visited = []
+#         parsed = json.loads(photos.decode('utf-8'))
+#         id = parsed['photos']['photo'][0]['id']
+#         geo = flickr.do_flickr_call('flickr.photos.geo.getLocation', photo_id=id)
 
 
 # dict = {location: [user1, user2]}
@@ -43,7 +43,7 @@ for lat in range(latidudes):
 # if (condition):
 #     nx.add_edge()
 
-def find_same_users(set1, set2):
-    return [user1, user2]
-
-users = len(find_same_users(photos1, photos2))
+# def find_same_users(set1, set2):
+#     return [user1, user2]
+#
+# users = len(find_same_users(photos1, photos2))
