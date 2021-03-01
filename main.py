db70345bb296f6342b2d0c56543c86df0c92818d
Veronika Blazhko <veronika.blazko@tuni.fi>
2021-03-01 16:36:08 +0200

    WIP

--- main.py
+++ main.py
@@ -1,30 +1,44 @@
 import flickrapi
 import json
+import requests
+import flickrapi
+import pandas as pd
+from tqdm import tqdm
+from vincenty import vincenty
+import matplotlib.pyplot as plt
+import time
+import numpy as np
 
 api_key = u'b766ae533b2843f748b174f53885eee4'
 api_secret = u'ac3884caa9690723'
 
 flickr = flickrapi.FlickrAPI(api_key, api_secret, format='json')
-photos = flickr.photos.search(per_page='250', bbox='20.410, 59.800, 30.920, 69.910', has_geo=1)
-parsed = json.loads(photos.decode('utf-8'))
-print(123)
-id = parsed['photos']['photo'][0]['id']
-geo = flickr.do_flickr_call('flickr.photos.geo.getLocation', photo_id=id)
-print(json.loads(geo.decode('utf-8')))
-parsed = json.loads(geo.decode('utf-8'))
 
-## 6242 pages, 10 photos per page
-## 50932, 10p/p
-## id | location | name | tags
+min_lat = 59.800
+max_lat = 69.910
+min_lon = 20.410
+max_lon = 30.920
 
-photos_set = []
-for i in range(1,40):
-    res = flickr.photos.search(per_page='250', bbox='60.562697, 21.402542, 69.766582, 28.664383', has_geo=1)
-    parsed = json.loads(photos.decode('utf-8'))
-    for p in parsed['photos']['photo']:
-        photos_set.append(p)
+n_lat = 6
+n_lon = 4
 
-len(photos_set)
+start_lat = min_lat
+start_lon = min_lon
+
+lats = np.arange(min_lat, max_lat, (max_lat - min_lat) / n_lat)
+lons = np.arange(min_lon, max_lon, (max_lon - min_lon) / n_lon)
+
+bboxes = []
+
+for i_lat, lat in enumerate(lats):
+    for i_lon, lon in enumerate(lons):
+        print('\n')
+        if i_lon + 1 != n_lon:
+            bbox = (lon, lat, lons[i_lon + 1], lats[i_lat + 1])
+
+        else:
+            bbox = (lon, lat, max_lon, max_lat)
+        print(bbox)
 # 50 lat x 50 lon -> at least 500 squares
 
 # for lat in range(latidudes):
